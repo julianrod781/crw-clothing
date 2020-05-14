3a7bd2dfe5a7c02ae943d7ff8aab95d310f28f40
Julianrod <julianrod781@hotmail.com>
2020-05-14 18:43:42 -0500

    adding performance optimizations to codebase with react lazy, suspense and memo

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import {
+  ErrorImageContainer,
+  ErrorImageOverlay,
+  ErrorImageText,
+} from "./error-boundary.styles";
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    //Process the error
+    return {
+      hasErrored: true,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/yW2W9SC.png" />
+          <ErrorImageText>Sorry this page is broken</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+
+    // https://i.imgur.com/yW2W9SC.png
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
